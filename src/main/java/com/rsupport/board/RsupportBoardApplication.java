70d99764d0a52912c0ec226728787e0eb1b0afa1
sujin_jeong <sujin_jeong@tmax.co.kr>
2025-05-30 20:08:27 +0900

    [chore] initial setting

---
+++ src/main/java/com/rsupport/board/RsupportBoardApplication.java
@@ -0,0 +1,13 @@
+package com.rsupport.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsupportBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsupportBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rsupport/board/RsupportBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsupport.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsupportBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
