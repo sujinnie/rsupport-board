52edd93db8bdc1868747494ead95efe7b658e25c
sujin_jeong <sujin_jeong@tmax.co.kr>
2025-06-01 05:36:32 +0900

    [feat] 공지 생성 관련 dto

---
+++ src/main/java/com/rsupport/board/notice/api/dto/NoticeResponseDTO.java
@@ -0,0 +1,43 @@
+package com.rsupport.board.notice.api.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class NoticeResponseDTO {
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private Long viewCount;
+    private AuthorDTO author; //작성자(==멤버,유저)
+    private List<AttachmentDTO> attachments;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class AuthorDTO {
+        private Long id;
+        private String name;
+        private String email;
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class AttachmentDTO {
+        private Long id;
+        private String filename;
+        private String url;
+        private LocalDateTime uploadedAt;
+    }
+}
