920e956dd121c590381c5c397f779e569e43e877
sujin_jeong <sujin_jeong@tmax.co.kr>
2025-06-01 02:48:56 +0900

    [feat] 스웨거 설정

--- src/main/java/com/rsupport/board/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/rsupport/board/common/exception/GlobalExceptionHandler.java
@@ -2,14 +2,12 @@ package com.rsupport.board.common.exception;
 
 import com.rsupport.board.common.dto.ResponseDTO;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.nio.file.AccessDeniedException;
 import java.util.Arrays;
